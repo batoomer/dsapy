10cd9baedacc3762c9409bf20651d408af91c74f
Batouchan Omer <batouchan.omer@gmail.com>
2022-12-01 16:27:04 +0200

    Changed insertion sort

--- dsapy/src/algorithms/sorting/insertion_sort.py
+++ dsapy/src/algorithms/sorting/insertion_sort.py
@@ -1,24 +1,109 @@
-def insertion_sort(data: list[int | float | str]) -> list[int | float | str]:
+import time
+
+
+class InsertionSort:
     """
-    This function is an implementation of the insertion sort algorithm.
-    It sorts the list in ascending order.
-    It returns a sorted copy of the input list.
+    This class is an implementation of the Insertion Sort Algorithm.\n
+    It has the following methods:
+        - sort: The main implementation of the sort algorithm
+        - animation: Returns a Dictionary of Lists, containing all swap and comparison operations.\n
+        - metadata: Returns a Dictionary of metadata for the algorithm.
 
-    :param data: list to be sorted
-    :return: sorted copy of input list
+    Animation Keys:
+        - swap
+        - comparison
     """
-    # Copy the input to a new list
-    data = data.copy()
 
-    # Loop through the list starting from the second element.
-    for i in range(1, len(data)):
-        j = i
+    def __init__(self):
+        """
+        Initializes the class with a metadata and an animation dictionary.
+        """
+        self.__metadata = {}
 
-        # Loop through the sorted part of the list and place the new element in the correct place
-        while j > 0 and data[j - 1] > data[j]:
-            # Swap
-            data[j], data[j - 1] = data[j - 1], data[j]
-            j -= 1
+        self.__animation = {}
 
-    # Return the sorted array
-    return data
+    def __init_metadata(self):
+
+        self.__metadata = {
+            'swap_count': 0,
+            'comparison_count': 0,
+            'iteration_count': 0,
+            'runtime': 0.0,
+        }
+
+        self.__animation = {
+            'swap': [],
+            'comparison': []
+        }
+
+    def metadata(self) -> dict:
+        """
+        Returns a metadata dictionary.\n
+        Metadata Keys:
+            - swap_count
+            - comparison_count
+            - iteration_count
+            - run_time
+
+        :return: A metadata dictionary
+        """
+        return self.__metadata
+
+    def animation(self) -> dict:
+        """
+        Returns a dictionary containing all swaps and animations.\n
+        Animation Keys:
+            - swap
+            - comparison
+
+        :return: An animation dictionary
+        """
+        return self.__animation
+
+    def sort(self, data: list[int | float | str]) -> list[int | float | str]:
+        """
+        This function is an implementation of the insertion sort algorithm.
+        It sorts the list in ascending order.
+        It returns a sorted copy of the input list.
+
+        :param data: list to be sorted
+        :return: sorted copy of input list
+        """
+
+        # Initiate/Reset the metadata and animations
+        self.__init_metadata()
+
+        # Copy the input to a new list
+        data = data.copy()
+
+        # CPU start time of the sorting algorithm
+        start_time = time.process_time()
+
+        # Loop through the list starting from the second element.
+        for i in range(1, len(data)):
+            self.__metadata['iteration_count'] += 1
+            j = i
+            # Loop through the sorted part of the list and place the new element in the correct place
+            while j > 0:
+                self.__metadata['comparison_count'] += 1
+                self.__animation['comparison'].append([j, j + 1])
+                # Transferred condition from while loop to make the comparisons animations
+                if data[j-1] <= data[j]:
+                    break
+
+                # REAL while loop starts from this line
+                self.__metadata['iteration_count'] += 1
+
+                # Swap
+                self.__metadata['swap_count'] += 1
+                self.__animation['swap'].append([j, j - 1])
+                data[j], data[j - 1] = data[j - 1], data[j]
+                j -= 1
+
+        # CPU end time of the sorting algorithm
+        end_time = time.process_time()
+        # Total CPU time in ms
+        self.__metadata['runtime'] = round(1000 * (end_time - start_time), 2)
+
+        # Return the sorted array
+        return data
--- dsapy/test/algorithms/sorting/test_insertion_sort.py
+++ dsapy/test/algorithms/sorting/test_insertion_sort.py
@@ -1,29 +1,31 @@
 import random
 from string import ascii_lowercase, digits
 import unittest
-from dsapy.src.algorithms.sorting.insertion_sort import insertion_sort
+from dsapy.src.algorithms.sorting.insertion_sort import InsertionSort
 
 
 class TestBubbleSort(unittest.TestCase):
+    sorter = InsertionSort()
+
     def test_sort_string(self):
         chars = ascii_lowercase + digits
         data = [''.join(random.choice(chars) for _ in range(10)) for _ in range(1000)]
 
-        sorted_data = insertion_sort(data)
+        sorted_data = self.sorter.sort(data)
         self.assertNotEqual(id(data), id(sorted_data))
         self.assertEqual(sorted(data), sorted_data)
 
     def test_sort_float(self):
         data = [round(random.uniform(-1000, 1000), 2) for _ in range(1000)]
 
-        sorted_data = insertion_sort(data)
+        sorted_data = self.sorter.sort(data)
         self.assertNotEqual(id(data), id(sorted_data))
         self.assertEqual(sorted(data), sorted_data)
 
     def test_sort_integers(self):
         data = [random.randint(-100, 100) for _ in range(1000)]
 
-        sorted_data = insertion_sort(data)
+        sorted_data = self.sorter.sort(data)
         self.assertNotEqual(id(data), id(sorted_data))
         self.assertEqual(sorted(data), sorted_data)
 
